74d61752351a777eedceea20af24d2fea31ab4e5
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-10-02 20:18:56 -0400

    Listening app

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ const app = express()
 const PORT = process.env.PORT
 const cors = require('cors')
 const morgan = require('morgan')
+
+
+
+app.get('/', (req, res)=>{
+  res.send('Hi World')
+})
+
+app.listen(PORT, ()=> console.log(`Listening on PORT ${PORT}`))
