47d830191fa5a6b4fd4d38ecff6f759e6622d698
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-10-13 21:31:54 -0400

    Working on routes

---
+++ routes/portfolio_routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router()
+
+const db = require('../models/Portfolio')
+
+router.get('/', async (req, res, next) =>{
+  try {
+    res.json(await db.find({}))
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+module.exports = router
